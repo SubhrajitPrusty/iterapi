6c6d05837a02926fb96cbd30fdbeee554dce7480
Subhrajit Prusty <subhrajit1997@gmail.com>
2019-08-28 17:57:37 +0530

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='iterapi',
-    version='1.1.4', 
+    version='1.1.5', 
     description='Python API to student portal of ITER',
 
     long_description=readme(),  

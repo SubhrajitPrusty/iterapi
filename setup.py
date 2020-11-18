32f1c060a6230efe63e274ce6dc29728b9fa5023
TheSpeedX <ggspeedx29@gmail.com>
2020-11-18 19:47:26 +0530

    bump subversion

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def readme():
 
 setup(
     name='iterapi',
-    version='1.2.1', 
+    version='1.2.2', 
     description='Python API to student portal of ITER',
 
     long_description=readme(),  
